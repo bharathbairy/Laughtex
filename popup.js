b8b5acba1b353580eecf9d031fdc32868ff18a29
Bharath Bairy <bharathb07@icloud.com>
2025-06-03 16:06:36 +0530

    added feature

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 document.getElementById('laughBtn').addEventListener('click', () => {
 
   const firstClick = document.querySelector('.firstClick');
+  const jokeBox = document.getElementById('jokeBox');
 
   fetch('https://icanhazdadjoke.com/', {
     headers: {
@@ -11,6 +12,7 @@ document.getElementById('laughBtn').addEventListener('click', () => {
     .then(data => {
       document.getElementById('joke').textContent = data.joke;
       firstClick.textContent = 'Next';
+      jokeBox.style.display = 'block';
     })
     .catch(e => {
       document.getElementById('joke').textContent = 'Oops! No joke for now 😢';
